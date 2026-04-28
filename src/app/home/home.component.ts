87fae25fd389d640f5480ddd2bf5dc02800f5bde
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-04-28 16:10:50 -0400

    interactuando, saludando y apuntando

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,8 +15,22 @@ export class HomeComponent {
     solicitud_selectedd: any = 1;
 
   options: AnimationOptions = {
-    path: 'https://lottie.host/5787ec7f-8813-48cf-9c7d-5baf944504c8/nhz0cdLQVX.json', // Asegúrate de que el nombre coincida
+    path: 'https://lottie.host/5787ec7f-8813-48cf-9c7d-5baf944504c8/nhz0cdLQVX.json', // normal esperando
   };
+  option2: AnimationOptions = {
+    path: 'https://lottie.host/058de9be-9c2f-4879-b699-4c9bcfe922a2/oS2zhobnv2.json', // saludando
+  };
+  option3: AnimationOptions = {
+    path: 'https://lottie.host/44d1b6a5-42dc-40e1-940a-41f105bda31d/YcRdaonEjg.json', // apuntando
+  };
+
+  cambiarASaludo() {
+  this.options = {
+    ...this.options,
+    path: 'https://lottie.host/058de9be-9c2f-4879-b699-4c9bcfe922a2/oS2zhobnv2.json',
+    loop: false // El saludo solo se hace una vez
+  };
+}
 
    optionSelected(value: number) {
       this.option_selectedd = value;
